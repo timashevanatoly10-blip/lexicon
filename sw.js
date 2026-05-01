efed4028038c5f478eab5ee6332cca660b4a18ba
timashevanatoly10-blip <timashevanatoly10@gmail.com>
2026-05-01 21:16:33 +0300

    Implement service worker for caching assets

--- sw.js
+++ sw.js
@@ -1,1 +1,38 @@
+const CACHE_NAME = "lexicon-pdf-ui-v1";
 
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      return cached || fetch(req);
+    })
+  );
+});
